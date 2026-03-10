5434ed54b1c1ab48a233592b05d27fbd68f630a7
najwa13 <najwafathi05@hotmail.com>
2026-03-10 10:32:58 +0000

    Add Challenge 7: The Contact Form (POST & Logic)

---
+++ Superglobals&FormProcessing/challenge7.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php 
+    $name='';
+    $email='';
+    $message='';
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $name=$_POST["name"];
+        $email=$_POST["email"];
+        $message=$_POST["message"];
+        if(str_contains($email, "@")==false){
+            echo "Email invalide!";
+        }
+    }
+    ?>
+    <h2>Contact Form</h2>
+    <form method="post">
+        Name : <input type="text" name="name" value="<?php echo "$name" ?>"><br>
+        E-mail :<input type="text" name="email"><br>
+        Message :<br>
+        <textarea name="message" ></textarea><br>
+        <input type="submit" value="Submit"><br>
+    </form>
+</body>
+</html>
