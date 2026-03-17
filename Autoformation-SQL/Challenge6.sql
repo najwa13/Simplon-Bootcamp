770642cded8e6a648e2daa6a50ccec92b33ae4b6
najwa13 <najwafathi05@hotmail.com>
2026-03-17 15:16:53 +0000

    Challenge #6: GROUP BY & HAVING

--- Autoformation-SQL/Challenge6.sql
+++ Autoformation-SQL/Challenge6.sql
@@ -1,9 +1,13 @@
 --1.Status Report:
-SELECT status,COUNT(title) AS total_books FROM library_books GROUP BY status;
+SELECT status,COUNT(title) AS total_books 
+FROM library_books
+ GROUP BY status;
 --2.Author Performance: 
-SELECT author,SUM(price) AS total_value FROM library_books GROUP BY author;
+SELECT author,SUM(price) AS total_value
+ FROM library_books 
+ GROUP BY author;
 --3.The Filter: 
 SELECT author,SUM(price) AS total_value 
 FROM library_books 
 GROUP BY author 
-HAVING total_value >= 300;
+HAVING total_value >= 500;
