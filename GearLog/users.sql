bd23e73f3f635f6bca544eb1d2b4a2e666584afc
najwa13 <najwafathi05@hotmail.com>
2026-03-24 15:14:45 +0100

    add all project files

---
+++ GearLog/users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL
+);
+INSERT INTO users (username, password) VALUES ('najwa', '12345');
